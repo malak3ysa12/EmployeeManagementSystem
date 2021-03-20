00e2011d9af57b9f2f32b82cb044f922d1656178
malak3ysa12 <53011237+malak3ysa12@users.noreply.github.com>
2021-03-20 04:16:29 +0200

    Update employeelogin.php

--- employeelogin.php
+++ employeelogin.php
@@ -1,3 +1,6 @@
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 <?php
 	session_start();
 if(isset($_SESSION['username']))
